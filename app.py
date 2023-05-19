7c7db34fef286521a95328da1498c3efee6c2c62
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-19 09:31:50 -0700

    refactor api class

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from api import API
 app = API()
 
 
+# The route method is a decorator that accepts a path and wraps the methods.
 @app.route('/home')
 def home(request, response):
     response.text = 'Hello from the HOME page'
