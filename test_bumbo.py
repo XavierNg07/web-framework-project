6679634b4a851f478a88f40b4b90df49f960c818
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-22 04:25:26 -0700

    added test for duplicate route

--- test_bumbo.py
+++ test_bumbo.py
@@ -10,4 +10,17 @@ def api():
 def test_basic_route_adding(api):
     @api.route('/home')
     def home(req, res):
-        res.text = 'YOLO'
+        res.text = 'the HOME page'
+
+
+def test_route_overlap_throws_exception(api):
+    @api.route('/home')
+    def home(req, res):
+        res.text = 'the HOME page'
+
+    with pytest.raises(AssertionError):
+        @api.route('/home')
+        def home2(req, res):
+            res.text = 'the HOME2 page'
+
+
