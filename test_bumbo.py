fc3cb3c734f3a06db86c249064accf7d96adac00
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-06-15 03:49:37 -0700

    added test for function based handlers

--- test_bumbo.py
+++ test_bumbo.py
@@ -174,3 +174,14 @@ def test_middleware_methods_are_called(api, client):
 
     assert process_request_called is True
     assert process_response_called is True
+
+
+def test_allowed_methods_for_function_based_handlers(api, client):
+    @api.route('/home', allowed_methods=['post'])
+    def home(req, res):
+        res.text = 'Hello from the HOME page'
+
+    with pytest.raises(AttributeError):
+        client.get('http://testserver/home')
+
+    assert client.post('http://testserver/home').text == 'Hello'
